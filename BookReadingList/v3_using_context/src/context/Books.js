0a59c04897d7d3ef75bd959f2376baecd783c9b9
nafeeziqbal <mdnafeez.iqbal@gmail.com>
2023-01-03 17:32:00 +0530

    Resolving ESLint warning on useEffect missing dependency

--- BookReadingList/v3_using_context/src/context/Books.js
+++ BookReadingList/v3_using_context/src/context/Books.js
@@ -1,4 +1,4 @@
-import { createContext, useState ,useEffect} from "react";
+import { createContext, useCallback, useState } from "react";
 import axios from "axios";
 
 
@@ -8,11 +8,10 @@ function Provider({children}){
 
     const [books,setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks =  useCallback(async () => {
     const response = await axios.get('http://localhost:3001/books');
-
     setBooks(response.data); 
-  }
+  },[]);
 
 
 
